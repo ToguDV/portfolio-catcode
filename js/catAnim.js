00e0f70fb5588397a3171f5f7eba3c3cdb1b1565
Andrey Villegas <timeofgetup@gmail.com>
2025-02-11 23:23:52 -0500

    Add spacecat animation

---
+++ js/catAnim.js
@@ -0,0 +1,33 @@
+const images = [
+        'img/about/4.jpg',
+    'img/about/spacecat.png'
+];
+
+const elementId = 'miElemento'; // Cambia esto por el ID de tu elemento
+const timePanik = 1000; // Alterna cada 2s después del primer cambio
+const timeRestore = 11000; // Alterna cada 2s después del primer cambio
+let index = 0;
+
+
+setTimeout(delay, 1000)
+
+function changeImage(img) {
+    const element = document.getElementById(elementId);
+    if (element) {
+        element.style.transition = 'background-image 0.7s ease-in-out';
+        element.style.backgroundImage = `url('${img}')`;
+    }
+}
+
+function delay() {
+    reactions();
+    setInterval(reactions, 15000); 
+}
+
+function reactions() {
+    setTimeout(changeImage, timePanik, images[1]);
+    setTimeout(changeImage, timeRestore, images[0]);
+}
+
+
+
